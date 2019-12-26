e3f8aa232d4e3fa17db2dccef51cc2e82286ec70
Jasterix <ljcalixte@gmail.com>
2019-12-26 17:01:16 -0500

    feature/mock jobs json

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./App.css";
 import Jobs from "./Jobs";
 
+const mockJobs = [
+  {
+    title: "SWE 1",
+    company: "blah"
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <Jobs />
+      <Jobs jobs={mockJobs} />
     </div>
   );
 }
