e1bc5de0a322c9052f04e10650475c2f268e04c4
Jasterix <ljcalixte@gmail.com>
2019-12-26 16:48:38 -0500

    feature/basic jobs component

---
+++ client/src/Jobs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function Jobs() {
+  return (
+    <div>
+      <Typography variant="h1">Entry Level Software Jobs</Typography>
+    </div>
+  );
+}
